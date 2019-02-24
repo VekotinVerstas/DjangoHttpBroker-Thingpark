940d68a05774d8419287d38b2d156d11b064003d
Aapo Rista <aapo.gitpub@rista.net>
2019-02-24 22:02:19 +0200

    Use routing_key from settings. Disable celery task for now.

--- thingpark/endpoints/thingpark.py
+++ thingpark/endpoints/thingpark.py
@@ -1,6 +1,7 @@
 import datetime
+from django.conf import settings
 from django.http.response import HttpResponse
-from broker.endpoint import EndpointProvider
+from broker.providers.endpoint import EndpointProvider
 from broker.utils import serialize_django_request, data_pack
 from thingpark.utils import get_datalogger, decode_json_body
 from thingpark.tasks import process_data
@@ -18,18 +19,17 @@ class ThingparkEndpoint(EndpointProvider):
         serialised_request['devid'] = devid
         serialised_request['time'] = datetime.datetime.utcnow().isoformat() + 'Z'
         packed_request = data_pack(serialised_request)
-        KEY_PREFIX = 'fvh.digita'  # TODO: from settings or endpoint config
+        pre = settings.RABBITMQ['ROUTING_KEY_PREFIX']
+        KEY_PREFIX = f'{pre}.thingpark'
         key = f'{KEY_PREFIX}.{devid}'
         send_message(packed_request, key)
-
         ok, body = decode_json_body(serialised_request['request.body'])
         if ok is False:
             return HttpResponse(f'JSON ERROR: {body}', status=400, content_type='text/plain')
         uplink = body.get('DevEUI_uplink')
         if uplink is not None:
-            # devid = uplink.get('DevEUI')
             datalogger, created = get_datalogger(devid=devid, update_activity=True)
             if devid is not None:
-                process_data.delay(devid, serialised_request)
-                # process_data.delay(devid, packed_request)
+                pass
+                # process_data.delay(devid, serialised_request)
         return HttpResponse('OK', content_type='text/plain')
