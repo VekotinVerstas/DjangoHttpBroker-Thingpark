69ad143fe2b2177230834d69992b523c5b6a4ffb
Aapo Rista <aapo.gitpub@rista.net>
2019-02-24 22:28:18 +0200

    Add thingpark decoder

---
+++ thingpark/management/commands/decode_thingpark_http.py
@@ -0,0 +1,72 @@
+import time
+import pytz
+import json
+from dateutil.parser import parse
+from django.conf import settings
+from influxdb.exceptions import InfluxDBClientError
+
+from broker.utils import data_unpack
+from broker.management.commands import RabbitCommand
+from broker.providers.decoder import DecoderProvider
+from thingpark.decoders._fvh import create_influxdb_obj, get_influxdb_client
+from thingpark.utils import decode_json_body, get_datalogger
+
+
+def parse_thingpark_request(serialised_request, data):
+    d = data['DevEUI_uplink']
+    devid = d['DevEUI']
+    datalogger, created = get_datalogger(devid=devid, update_activity=False)
+    timestamp = parse(d['Time'])
+    timestamp = timestamp.astimezone(pytz.UTC)
+    payload_hex = d['payload_hex']
+    rssi = d['LrrRSSI']
+
+    plugins = DecoderProvider.get_plugins({})
+    for plugin in plugins:
+        decoder_name = f'{plugin.app}.{plugin.name}'
+        if datalogger.decoder == decoder_name:
+            idata = plugin.decode_payload(payload_hex)
+            break
+
+    idata['rssi'] = rssi
+    keys_str = 'aqburk'
+    measurement = create_influxdb_obj(devid, keys_str, idata, timestamp)
+    measurements = [measurement]
+    # print(json.dumps(measurements, indent=1))
+    dbname = 'aqburk'
+    iclient = get_influxdb_client(database=dbname)
+    iclient.create_database(dbname)
+    try:
+        iclient.write_points(measurements)
+        return True
+    except InfluxDBClientError as err:
+        return False
+
+
+def consumer_callback(channel, method, properties, body):
+    serialised_request = data_unpack(body)
+    ok, data = decode_json_body(serialised_request['request.body'])
+    if 'DevEUI_uplink' in data:
+        parse_thingpark_request(serialised_request, data)
+    print(data)
+    # print('Got a message:', data_unpack(body))
+    time.sleep(2)
+    print('Acked')
+    time.sleep(2)
+    channel.basic_ack(method.delivery_tag)
+    print('end')
+
+
+class Command(RabbitCommand):
+    help = 'Decode thingpark'
+
+    def add_arguments(self, parser):
+        # parser.add_argument('keys', nargs='+', type=str)
+        super().add_arguments(parser)
+        pass
+
+    def handle(self, *args, **options):
+        options['routing_key'] = f'{settings.RABBITMQ["ROUTING_KEY_PREFIX"]}.thingpark.#'
+        options['queue_name'] = 'decode_thingpark_http_queue'
+        options['consumer_callback'] = consumer_callback
+        super().handle(*args, **options)
