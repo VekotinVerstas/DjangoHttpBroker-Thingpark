53c98d988b8ae7710280e3cd53ce5f7edc0bd99e
Aapo Rista <aapo.gitpub@rista.net>
2019-02-27 15:24:11 +0200

    Add location and address fields to Datalogger

--- thingpark/models.py
+++ thingpark/models.py
@@ -1,5 +1,8 @@
+import json
 from django.db import models
+from django.core.exceptions import ValidationError
 from broker.providers.decoder import DecoderProvider
+from broker.models import Forward
 
 decoders = DecoderProvider.get_plugins()
 DECODER_HANDLER_CHOICES = [(f'{a.app}.{a.name}', f'{a.app}.{a.name}') for a in decoders]
@@ -10,6 +13,16 @@ class Datalogger(models.Model):
     name = models.CharField(max_length=256, blank=True)
     description = models.CharField(max_length=10000, blank=True)
     decoder = models.CharField(max_length=128, blank=True, choices=DECODER_HANDLER_CHOICES)
+    forwards = models.ManyToManyField(Forward,
+                                      blank=True,
+                                      through="DataloggerForward",
+                                      related_name="related_dataloggers",
+                                      verbose_name="Data to forward")
+    lat = models.FloatField(blank=True, null=True)
+    lon = models.FloatField(blank=True, null=True)
+    country = models.CharField(max_length=256, blank=True)
+    locality = models.CharField(max_length=256, blank=True)
+    street = models.CharField(max_length=256, blank=True)
     activity_at = models.DateTimeField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -18,10 +31,26 @@ class Datalogger(models.Model):
         return f'{self.devid} ({self.decoder})'
 
 
-class LorawanMsg(models.Model):
+class DataloggerForward(models.Model):
     datalogger = models.ForeignKey(Datalogger, on_delete=models.CASCADE)
-    rssi = models.CharField(max_length=256, blank=True)
-    received_at = models.DateTimeField(auto_now_add=True)
+    forward = models.ForeignKey(Forward, on_delete=models.CASCADE, related_name='related_forwards')
+    config = models.TextField(default='{}', help_text='All required configuration parameters in JSON format')
 
     def __str__(self):
-        return f'{self.datalogger}'
+        return f'{self.datalogger} -> {self.forward}'
+
+    def clean(self, exclude=None):
+        try:
+            config = json.loads(self.config)
+        except json.JSONDecodeError as err:
+            raise ValidationError(f'JSON error in config: {err}')
+        self.config = json.dumps(config, indent=1)
+
+
+# class LorawanMsg(models.Model):
+#     datalogger = models.ForeignKey(Datalogger, on_delete=models.CASCADE)
+#     rssi = models.CharField(max_length=256, blank=True)
+#     received_at = models.DateTimeField(auto_now_add=True)
+#
+#     def __str__(self):
+#         return f'{self.datalogger}'
