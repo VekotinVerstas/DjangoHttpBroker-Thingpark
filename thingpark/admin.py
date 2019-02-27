ec3bb730f9aa84f722182fef553812eb3811fdbd
Aapo Rista <aapo.gitpub@rista.net>
2019-02-27 16:12:01 +0200

    Add ForwardInline

--- thingpark/admin.py
+++ thingpark/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Datalogger
+from .models import Datalogger, DataloggerForward
+
+
+class ForwardInline(admin.StackedInline):
+    model = DataloggerForward
+    extra = 1
 
 
 class DataloggerAdmin(admin.ModelAdmin):
+    inlines = [ForwardInline]
     list_display = ('devid', 'name', 'activity_at', 'created_at')
 
 
