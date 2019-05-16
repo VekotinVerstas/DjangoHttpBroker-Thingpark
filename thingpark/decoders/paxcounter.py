d830e4ad97f809d3157ea3c69002195a7c0ee329
Aapo Rista <aapo.gitpub@rista.net>
2019-05-16 09:37:37 +0300

    Make port positional argument

--- thingpark/decoders/paxcounter.py
+++ thingpark/decoders/paxcounter.py
@@ -5,5 +5,6 @@ from thingpark.parsers.paxcounter import parse_paxcounter
 class PaxcounterDecoder(DecoderProvider):
     description = 'Decode PAXCOUNTER payload'
 
-    def decode_payload(self, payload_hex, **kwargs):
-        return parse_paxcounter(payload_hex, port=kwargs['port'])
+    def decode_payload(self, payload_hex, port, **kwargs):
+        data = parse_paxcounter(payload_hex, port)
+        return data
