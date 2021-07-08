d43afa3b52fd4b7255008ac7fbed2baee55e2865
Aapo Rista <aapris@gmail.com>
2021-07-08 13:40:31 +0300

    Add sound

--- thingpark/parsers/elsys.py
+++ thingpark/parsers/elsys.py
@@ -3,25 +3,30 @@ def get_value(hex_str, data):
         end = 6
         data['temp'] = int(hex_str[2:end], 16) / 10
         hex_str = hex_str[end:]
-    if hex_str[:2] == '02':
+    elif hex_str[:2] == '02':
         end = 4
         data['humi'] = int(hex_str[2:end], 16)
         hex_str = hex_str[end:]
-    if hex_str[:2] == '04':
+    elif hex_str[:2] == '04':
         end = 6
         data['lux'] = int(hex_str[2:end], 16)
         hex_str = hex_str[end:]
-    if hex_str[:2] == '05':
+    elif hex_str[:2] == '05':
         end = 4
         data['motion'] = int(hex_str[2:end], 16)
         hex_str = hex_str[end:]
-    if hex_str[:2] == '06':
+    elif hex_str[:2] == '06':
         end = 6
         data['co2'] = int(hex_str[2:end], 16)
         hex_str = hex_str[end:]
-    if hex_str[:2] == '07':
+    elif hex_str[:2] == '07':
         end = 6
         data['volt'] = int(hex_str[2:end], 16) / 1000
+        hex_str = hex_str[end:]
+    elif hex_str[:2] == '15':
+        end = 6
+        data['soundPeak'] = int(hex_str[2:4], 16)
+        data['soundAvg'] = int(hex_str[4:end], 16)
         hex_str = hex_str[end:]
     else:
         data['error'] = hex_str
@@ -44,3 +49,24 @@ def parse_elsys(hex_str, port=None):
     while len(hex_str) > 0:
         hex_str, data = get_value(hex_str, data)
     return data
+
+
+def decode_hex(hex_str: str, port: int = None):
+    return parse_elsys(hex_str, port=port)
+
+
+if __name__ == '__main__':
+    import sys
+
+    try:
+        print(decode_hex(sys.argv[1], sys.argv[2]))
+    except IndexError as err:
+        print('Some examples:')
+        for s in [('0100d3020c04000b05000601aa070e41', 5),
+                  ('0100e50212040041050006003a070e4f', 5),
+                  ('0100c202100402d205000601cc070e49', 5),
+                  ('0100ef024704010a0501070e04155326', 5),  # ERS Sound
+                  ]:
+            print(decode_hex(s[0], s[1]))
+
+        print(f'\nUsage: {sys.argv[0]} hex_payload port\n\n')
